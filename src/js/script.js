ba2fc26c4c9e2734932cd30a02839bc7f2589576
Eremeev_Alexander <sanya.eremey@mail.ru>
2023-01-06 14:48:27 +0300

    create reviews block with comments

--- src/js/script.js
+++ src/js/script.js
@@ -181,3 +181,125 @@ if (pageCatalog) {
     }
   })
 }
+
+
+// Появление крестика при наборе текста и удаление текста
+const presearch = document.querySelector('[data-presearch]');
+
+if (presearch) {
+
+  const presearchInputField = presearch.querySelector('[data-presearch-input]');
+  const presearchResetBtn = presearch.querySelector('[data-presearch-reset]');
+
+  // удаление текста
+  presearch.addEventListener('click', (evt) => {
+    if (evt.target.matches('[data-presearch-reset]')) {
+      if (presearchInputField.value !== "") {
+        presearchInputField.value = "";
+        presearchResetBtn.classList.remove('is-active');
+      }
+    }
+  })
+
+  // Появление кнопки сброса набранного текста
+  presearchInputField.addEventListener('input', () => {
+    if (presearchInputField.value !== "") {
+      presearchResetBtn.classList.add('is-active');
+    } else {
+      presearchResetBtn.classList.remove('is-active');
+    }
+  })
+}
+
+// Табы - карточка товара
+
+const productData = document.querySelector('[data-product-data]');
+
+if (productData) {
+
+  // Из Node List делаем массив с помощью Array.from, чтобы работал метод indexOf()
+  const tabs = Array.from(productData.querySelectorAll('[data-tab-btn]'));
+  const tabContents = productData.querySelectorAll('[data-tab-content]');
+
+  // Инициализация активных эл-ов
+  tabs[0].classList.add('is-active');
+  tabContents[0].classList.add('is-active');
+
+  productData.addEventListener('click', (evt) => {
+    if (evt.target.matches('[data-tab-btn]')) {
+      const activeTab = evt.target;
+      const indexActiveTab = tabs.indexOf(activeTab);
+
+
+      tabs.forEach((tab) => {
+        tab.classList.remove('is-active');
+      })
+      activeTab.classList.add('is-active');
+
+
+      tabContents.forEach((content) => {
+        content.classList.remove('is-active');
+      })
+      tabContents[indexActiveTab].classList.add('is-active');
+    }
+  })
+}
+
+
+// Аккордеон в комментариях
+
+const userReviewBlock = document.querySelector('[data-reviews]');
+
+if (userReviewBlock) {
+
+  // Ответ на отзыв
+  userReviewBlock.addEventListener('click', (evt) => {
+    if (evt.target.matches('[data-user-comments]')) {
+      const userCommentsShowBtn = evt.target;
+      const currentUserReviewBlock = userCommentsShowBtn.closest('[data-user-review]');
+      const commentsWrapper = currentUserReviewBlock.querySelector('[data-comments-wrapper]');
+
+      commentsWrapper.classList.toggle('is-open');
+      userCommentsShowBtn.classList.toggle('is-open');
+
+
+      if (commentsWrapper.classList.contains('is-open')) {
+        commentsWrapper.style.maxHeight = commentsWrapper.scrollHeight + 'px';
+      } else {
+        commentsWrapper.classList.remove('is-open');
+        userCommentsShowBtn.classList.remove('is-open');
+        commentsWrapper.style.maxHeight = 0;
+
+      }
+    }
+
+    // Ответ на коммент
+    if (evt.target.matches('[data-comment-answer-btn]')) {
+      const answerCommentBtn = evt.target;
+
+      const currentUserReviewBlock = answerCommentBtn.closest('[data-user-review]');
+      const commentsWrapper = currentUserReviewBlock.querySelector('[data-comments-wrapper]');
+
+      const commentItem = answerCommentBtn.closest('[data-user-comment]');
+      const commentFooter = commentItem.querySelector('[data-comment-footer]');
+      const commentsAnswerForm = commentItem.querySelector('[data-answer-form]');
+
+      commentsAnswerForm.classList.toggle('is-open');
+      commentFooter.classList.toggle('is-open');
+
+      if (commentsAnswerForm.classList.contains('is-open')) {
+        commentsAnswerForm.style.maxHeight = commentsAnswerForm.scrollHeight + 'px';
+        commentsWrapper.style.maxHeight = '100%';
+        console.log(commentsWrapper)
+      } else {
+        commentsAnswerForm.classList.remove('is-open');
+        commentFooter.classList.remove('is-open');
+        commentsAnswerForm.style.maxHeight = 0;
+
+      }
+    }
+  })
+}
+
+
+
