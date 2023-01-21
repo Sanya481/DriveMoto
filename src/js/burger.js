62f517dc3a7d53e0ff3548d38458def0e6708b66
Eremeev_Alexander <sanya.eremey@mail.ru>
2023-01-21 21:20:56 +0300

    create mobile-nav

--- src/js/burger.js
+++ src/js/burger.js
@@ -1,11 +1,21 @@
 if (window.matchMedia('screen and (max-width: 767px)').matches) {
+  const body = document.body;
+  const header = document.querySelector('[data-header]');
+  const mobileNav = header.querySelector('[data-mobile-nav]');
   const burger = document.querySelector('[data-burger]');
-
+  const burgerCircle = burger.querySelector('[data-burger-circle]');
 
   burger.addEventListener('click', (evt) => {
     evt.preventDefault();
 
+    body.classList.toggle('scroll-lock');
+    // header.classList.toggle('is-show');
+    burger.classList.toggle('is-show');
+
     burger.classList.toggle('is-open');
+    burgerCircle.classList.toggle('is-expand');
+    mobileNav.classList.toggle('is-open');
+
   })
 
 }
